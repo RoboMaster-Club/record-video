a6a55c4b408ad7317d77a4528ed25f7a58d9b83d
williamswhy <why980429@gmail.com>
2019-06-23 18:11:48 -0400

    exit with ctrl-c

--- record-video.cpp
+++ record-video.cpp
@@ -1,8 +1,23 @@
+#include <signal.h>
+#include <unistd.h>
+#include <atomic>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
 
+std::atomic<bool> quit(false);    // signal flag
+
+void got_signal(int) {
+    quit.store(true);
+}
+
 int main(int argc, char** argv) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = got_signal;
+    sigfillset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, NULL);
+
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_COLOR, 848, 480, RS2_FORMAT_BGR8, 60);
     // Start streaming with default recommended configuration
@@ -16,12 +31,15 @@ int main(int argc, char** argv) {
     cv::VideoWriter writer(argv[1], CV_FOURCC('m', 'p', '4', 'v'), 60, cv::Size(848, 480));
     cv::Mat img = cv::Mat(cv::Size(image_width, image_height), CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
     while(true) {
-        imshow("image", img);
         writer.write(img);
+#ifndef NDEBUG
+        imshow("image", img);
         cv::waitKey(1);
         char c = (char) cv::waitKey(1);
         if (c == 27)
             break;
+#endif
+        if (quit.load()) break;
         data = pipe.wait_for_frames();
         color = data.get_color_frame();
         img = cv::Mat(cv::Size(image_width, image_height), CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
