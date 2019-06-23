7199be561925dc8adecfed410e002f594404ad51
williamswhy <why980429@gmail.com>
2019-06-23 18:02:39 -0400

    fix fourcc

--- record-video.cpp
+++ record-video.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
 
@@ -12,17 +13,18 @@ int main(int argc, char** argv) {
     rs2::video_frame color = data.get_color_frame();
     int image_width = color.get_width();
     int image_height = color.get_height();
-
-    cv::VideoWriter writer(argv[0], 0x00000021, 60, cv::Size(848, 480));
-
+    cv::VideoWriter writer(argv[1], CV_FOURCC('m', 'p', '4', 'v'), 60, cv::Size(848, 480));
+    cv::Mat img = cv::Mat(cv::Size(image_width, image_height), CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
     while(true) {
-        cv::Mat img = cv::Mat(cv::Size(image_width, image_height), CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
         imshow("image", img);
         writer.write(img);
         cv::waitKey(1);
         char c = (char) cv::waitKey(1);
         if (c == 27)
             break;
+        data = pipe.wait_for_frames();
+        color = data.get_color_frame();
+        img = cv::Mat(cv::Size(image_width, image_height), CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
     }
     pipe.stop();
     cfg.disable_all_streams();
